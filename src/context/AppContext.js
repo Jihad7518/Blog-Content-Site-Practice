d507d1502b5a2d4ee5db3c4fa98bed6ee8fcb98e
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-08 04:23:38 +0600

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,14 @@
 
+import { createContext, useState } from "react";
+import {baseUrl} from "../baseUrl";
+
+
+export const AppContext = createContext();
+
+export default function AppContextProvider({children}) {
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(null);
+
+    //data filling
