2c994d8f5e164159b0109e2cc889a44b518f3a71
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-08 04:18:05 +0600

    Update Blogs.js

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,1 +1,22 @@
 B
+
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext';
+import Spinner from './Spinner';
+import "./Blogs.css";
+
+const Blogs = () => {
+  //consuming
+  const {posts, loading} = useContext(AppContext);
+
+
+
+  return (
+    <div className='w-11/12 max-w-[670px] h-screen py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px] felx justify-center items-center'>
+
+    {
+      loading ? 
+
+      (<Spinner/>) : 
+      (
+        posts.length === 0 ? 
