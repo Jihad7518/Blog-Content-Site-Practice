307ea8d00d2d4855cf9246195456cd8da4dd0878
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-08 04:23:01 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,3 +13,16 @@ export default function App() {
   useEffect( () => {
     fetchBlogPosts();
   }, []);
+
+  return (
+    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+
+      <Header/>
+
+      <Blogs/>
+
+      <Pagination/>
+
+    </div>
+  );
+}
