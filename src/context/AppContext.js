c5bd2d9fef216c17d7edd3614863631e4dd9003b
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-08 04:23:51 +0600

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -12,3 +12,31 @@ export default function AppContextProvider({children}) {
     const [totalPages, setTotalPages] = useState(null);
 
     //data filling
+async function fetchBlogPosts(page = 1) {
+        setLoading(true);
+
+        let url = `${baseUrl}?page=${page}`;
+
+
+        try {
+            const result = await fetch(url);    
+            const data = await result.json();
+            console.log(data);
+            setPage(data.page);
+            setPosts(data.posts);
+            setTotalPages(data.totalPages);
+        }
+        catch (error) {
+            console.log("Error in fetching data");
+            setPage(1);
+            setPosts([]);
+            setTotalPages(null);
+        }
+
+        setLoading(false);
+    }
+
+    function handlePageChange(page) {
+        setPage(page);
+        fetchBlogPosts(page);
+    }
