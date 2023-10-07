2a0f5239965b1cd21bb7b2b5e703c0e6e9b89302
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-08 04:22:45 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React, { useContext, useEffect } from "react";
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import Pagination from "./components/Pagination";
+import { AppContext } from "./context/AppContext";
+import "./App.css";
 
+
+export default function App() {
+
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect( () => {
+    fetchBlogPosts();
+  }, []);
